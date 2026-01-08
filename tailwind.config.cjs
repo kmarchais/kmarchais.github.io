b6b93271fdac6bd662caab965bc1f46bf0d69d17
Kevin Marchais <kevinmarchais@gmail.com>
2026-01-08 02:51:46 +0100

    Fix Tailwind content config to include TypeScript files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,mdx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,mdx}"],
   mode: "jit",
   theme: {
     extend: {
